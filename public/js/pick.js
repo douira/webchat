a872b59a85c71fd96f0174ce7754591670ab5229
douira <douira100@gmail.com>
2017-04-17 18:48:24 +0200

    username validation client side, incomplete

--- public/js/pick.js
+++ public/js/pick.js
@@ -1,3 +1,25 @@
-window.onload = function() {
+var lastChecked = -Infinity;
+var isScheduled = false;
+var minTimeBetween = 1000;
 
+window.onload = function() {
+  var el = document.getElementById("nameField");
+  el.oninput = checkInputField;
+  el.onpropertychange = checkInputField;
 };
+
+function checkInputField(event) {
+  var currentTime = Date.now();
+  if (currentTime - lastChecked > minTimeBetween) {
+    lastChecked = currentTime;
+    var element = event.target;
+    var value = element.value;
+    console.log();
+  } else if (! isScheduled) {
+    isScheduled = true;
+    window.setTimeout(function() {
+      isScheduled = false;
+      checkInputField(event);
+    }, currentTime - lastChecked);
+  }
+}
