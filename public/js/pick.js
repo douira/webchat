ae1a2e9ac77046eb955f860bccd1f58f5710cd6f
douira <douira100@gmail.com>
2017-04-30 15:55:06 +0200

    commenting and js can move to nain page

--- public/js/pick.js
+++ public/js/pick.js
@@ -1,75 +1,126 @@
-var lastChecked = -Infinity;
-var checkIsScheduled = false;
-var minTimeBetween = 1000;
-var states = ["success", "warning", "danger"];
-var warningMessage = "";
-var active = -1;
-var module = {};
-var buttonEnabled = false;
+var lastChecked = -Infinity; //timestamp of checking the name's validity
+var checkIsScheduled = false; //set to true when waiting for the server to respond with name validity
+var minTimeBetween = 1000; //time between validating the name with the server
+var states = ["success", "warning", "danger"]; //possible states of name validity
+var warningMessage = ""; //specific warning message displayed for the warning state
+var active = -1; //which one of the states is active, also determines the color and icon of the view field
+var module = {}; //fake node module for using usernameValidator.js
+var buttonEnabled = false; //disabled the button until name is validated
+var value = ""; //current contents of the name field
 
+//setup as soon as we can be sure that everything we need is in place
 window.onload = function() {
+  //get the input field and attach change handlers
   var el = $("#name-field")[0];
   el.oninput = checkInputField;
   el.onpropertychange = checkInputField;
+
+  //do initital check
   checkInputField({target: el});
+
+  //attach click handler on button
+  $("#submit-button").click(sendForm);
 };
 
+//actually registers the chosen name and moves the browser back to the main chat page
+function sendForm() {
+  //send name to /registername to select it for the user's session
+  $.post("/registername", {
+    name: value
+  });
+
+  //more browser to main page
+  window.location.href = "/";
+}
+
+//handler for change of the name inout field
 function checkInputField(event) {
-  var value = event.target.value.trim();
+  //get the entered user name
+  value = event.target.value.trim();
+
+  //return trimmed user name, user shouldn't have to remove spaced manually
   event.target.value = value;
+
+  //get preliminary name validation, only name itself
   var validationResponse = module.exports(value);
+
+  //if validation response if something, then the name is invalid
   if (validationResponse) {
     warningMessage = validationResponse;
     active = 1;
     updateDisplay();
     setCheckSchedulingStatus(false);
-  } else {
-    //we need to ask the server
+  } else { //name is ok, we need to ask the server now
+    //we're waiting for a server response now or are going to send server request in a while
+    setCheckSchedulingStatus(true);
+
+    //get current time and check if we shoudl aks the server now or wait
     var currentTime = Date.now();
     if (currentTime - lastChecked > minTimeBetween) {
-      setCheckSchedulingStatus(true);
+
+
+      //now is the last time we checked because we're doing it right now
       lastChecked = currentTime;
+
+      //send get request to validation endpoint
       $.get("/validateusername/" + value, function(res) {
+        //not waiting anymore, we have response now
         setCheckSchedulingStatus(false);
+
+        //set active state to responded state
         active = states.indexOf(res);
+
+        //if the server responds with a warning then the client validated the name differently from the server, use inconclusive warning message
         if (active === 1) {
           warningMessage = "invalid format";
         }
+
+        //update display with new state
         updateDisplay();
       });
     } else if (! checkIsScheduled) {
-      setCheckSchedulingStatus(true);
-      console.log("postponed check");
+      //set a timer to check again in the remaining time we have to wait
       window.setTimeout(function() {
         checkInputField(event);
       }, minTimeBetween - (currentTime - lastChecked));
-    } else {
-      console.log("already waiting for check");
     }
   }
 }
 
+//sets wether or not we're waiting for the server to respond
 function setCheckSchedulingStatus(state) {
   checkIsScheduled = state;
-  console.log("updated to", state);
   updateButton();
 }
 
+//updates the disabled state of the button according to the set state
 function updateButton() {
+  //disabled when we're waiting or the name isn't ok
   $("#submit-button").attr("disabled", active || checkIsScheduled ? " " : null);
 }
 
+//updates the name field and the info text
 function updateDisplay() {
+  //get the elements we want to work with
   var nameField = $("#name-field");
   var groupElement = $("#username-form-group");
+
+  //remove inactive states and add the active state
   states.forEach(function(state, index) {
+    //is true when the current state is the active one
     var isActive = index === active;
+
+    //remove or add the current state styles
     $("#" + state + "-feedback")[0].style.display = isActive ? "block" : "none";
     groupElement[isActive ? "addClass" : "removeClass"]("has-" + state);
     nameField[isActive ? "addClass" : "removeClass"]("form-control-" + state);
   });
+
+  //add wanring reason text if applicable
   if (active === 1) {
     $("#warning-reason").text(warningMessage);
   }
+
+  //update the button state with everythign else, active state may have changed as cause for calling this function
   updateButton();
 }
--- routes/chat.js
+++ routes/chat.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const usernameValidator = require("../public/js/usernameValidator");
 
 const exp = module.exports = {
   router: router
 };
 
-const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vel facilisis nisl, eu porta ex. Cras sit amet lacus pretium, ornare augue ac, laoreet metus. Duis eleifend rhoncus nisl, vitae mollis ipsum efficitur et. Curabitur turpis neque, mattis eu tincidunt ut, pretium quis felis. Pellentesque maximus vehicula dui, in consectetur risus ultrices eu. Sed finibus lacinia quam a auctor. Suspendisse non blandit justo. Praesent eu lorem sed lorem ullamcorper consectetur ac vel lectus. Quisque varius massa id semper venenatis. Etiam vehicula quam in quam sollicitudin maximus. Vestibulum eu blandit nisl. Integer eget mattis dolor, et imperdiet libero. Fusce eu diam quis mi posuere egestas pretium a mauris. Praesent molestie fringilla augue, vitae molestie massa varius eu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque sit amet molestie augue. In fermentum nisl nunc, vitae lacinia felis convallis eget. Phasellus mi leo, pretium in efficitur quis, eleifend in est. Pellentesque rhoncus lorem non nisi finibus rhoncus. Integer sed ligula id mauris fermentum mollis eget sed odio. Nunc nibh enim, viverra et nulla ut, euismod tempus dolor. Mauris volutpat pulvinar est, quis viverra felis pellentesque eu. Donec gravida justo quis diam finibus, vel malesuada lorem ultrices. Etiam sit amet felis.";
+/*const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vel facilisis nisl, eu porta ex. Cras sit amet lacus pretium, ornare augue ac, laoreet metus. Duis eleifend rhoncus nisl, vitae mollis ipsum efficitur et. Curabitur turpis neque, mattis eu tincidunt ut, pretium quis felis. Pellentesque maximus vehicula dui, in consectetur risus ultrices eu. Sed finibus lacinia quam a auctor. Suspendisse non blandit justo. Praesent eu lorem sed lorem ullamcorper consectetur ac vel lectus. Quisque varius massa id semper venenatis. Etiam vehicula quam in quam sollicitudin maximus. Vestibulum eu blandit nisl. Integer eget mattis dolor, et imperdiet libero. Fusce eu diam quis mi posuere egestas pretium a mauris. Praesent molestie fringilla augue, vitae molestie massa varius eu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque sit amet molestie augue. In fermentum nisl nunc, vitae lacinia felis convallis eget. Phasellus mi leo, pretium in efficitur quis, eleifend in est. Pellentesque rhoncus lorem non nisi finibus rhoncus. Integer sed ligula id mauris fermentum mollis eget sed odio. Nunc nibh enim, viverra et nulla ut, euismod tempus dolor. Mauris volutpat pulvinar est, quis viverra felis pellentesque eu. Donec gravida justo quis diam finibus, vel malesuada lorem ultrices. Etiam sit amet felis.";
 
 const messages = Array(40).fill(0).map(() => {
   const start = Math.floor(Math.random() * text.length / 2);
@@ -18,27 +17,26 @@ const messages = Array(40).fill(0).map(() => {
     author: text.substr(namePos, nameLength).trim(),
     time: new Date(Math.round(Math.random() * Date.now()))
   };
-});
+});*/
 
 router.get("/", function(req, res, next) {
-  res.render("chat", {
-    messages: exp.db.messages
-  });
-});
-
-router.post("/", function(req, res, next) {
-  const userName = req.body.name;
-  const validationResponse = usernameValidator(userName);
-  if (validationResponse) {
+  const sess = req.session;
+  console.log("get chat", req.session);
+  if (sess.nameError) {
     res.render("chat", {
+      title: "webchat error",
       messages: exp.db.messages,
-      errorMessage: "user name erro: " + validationResponse
+      errorMessage: "user name error: " + sess.nameError
+    });
+  } else if (sess.userName) {
+    res.render("chat", {
+      title: "webchat @" + sess.userName,
+      messages: exp.db.messages,
+      userName: sess.userName
     });
   } else {
     res.render("chat", {
-      title: "webchat @" + userName,
-      messages: exp.db.messages,
-      userName: userName
+      messages: exp.db.messages
     });
   }
 });
